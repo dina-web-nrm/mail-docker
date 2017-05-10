2bf1d9f60a2b3e5c56b99d7b43c2735779d76910
Ingimar E <inkimar@gmail.com>
2017-05-10 13:59:14 +0200

    dkim script

--- dkim_setup.sh
+++ dkim_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# what is dkim , http://www.dkim.org/
 # https://github.com/tomav/docker-mailserver/wiki/Configure-DKIM 
 # https://addons.mozilla.org/en-US/thunderbird/addon/dkim-verifier/
 docker run --rm \
