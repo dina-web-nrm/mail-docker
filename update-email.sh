20b400fabd6b9a0a48b4df5cff8ecb21bb0c77ec
Markus Skyttner <markus.skyttner@nrm.se>
2017-09-04 08:29:13 -0400

    for updating password for users, unpackage the .gpg-file before usage

--- update-email.sh
+++ update-email.sh
@@ -1,3 +1,8 @@
-./setup.sh -i tvial/docker-mailserver:2.2 -c mail.dina-web.net email update <user>@mail.dina-web.net <psw> 
-docker-compose restart mail
-docker-compose logs --tail=30 -f mail
+#!/bin/bash
+while IFS=, read USER PSW
+do
+	echo "user is $USER and password is $PSW"
+	./setup.sh -i tvial/docker-mailserver:2.2 -c mail.dina-web.net email update $USER@mail.dina-web.net $PSW 
+	docker-compose restart mail
+	docker-compose logs --tail=30 -f mail
+done < users.csv
