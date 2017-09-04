d786b2a7f1af7e4531d6f3e46cf7b86e1e8f27cb
Markus Skyttner <markus.skyttner@nrm.se>
2017-09-04 08:31:51 -0400

    removing an 'echo'

--- update-email.sh
+++ update-email.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 while IFS=, read USER PSW
 do
-	echo "user is $USER and password is $PSW"
 	./setup.sh -i tvial/docker-mailserver:2.2 -c mail.dina-web.net email update $USER@mail.dina-web.net $PSW 
 	docker-compose restart mail
 	docker-compose logs --tail=30 -f mail
